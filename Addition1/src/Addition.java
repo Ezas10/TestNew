4a4aa9069013d58b6fb05e09596a4c47df3410db
Ezas10 <shaikezas10@gmail.com>
2022-09-06 17:13:15 +0530

    This first commit

---
+++ Addition1/src/Addition.java
@@ -0,0 +1,17 @@
+
+	import java.util.Scanner;
+	public class Addition {
+		public static void main(String[]arge) {
+			int a,b,c,d;
+			System.out.println("Enter the numbers are three steps:");
+			try (Scanner sc=new Scanner(System.in)){
+			a=sc.nextInt();
+			b=sc.nextInt();
+			c=sc.nextInt();
+	        d=a+b+c;
+			System.out.println("Sum of the values:" +d);
+		System.out.println(" ");
+		}
+	}}
+
+
